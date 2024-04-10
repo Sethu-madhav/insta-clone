68e55e1f347a6123d86d580229a4f31929c235ec
Sethu-madhav <sethu4784@gmail.com>
2024-04-10 12:14:33 -0400

    add reeact roter, icons, auth and home pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
-import {Button} from '@chakra-ui/react'
+import {Routes, Route} from 'react-router-dom'
+import HomePage from './pages/HomePage/HomePage'
+import AuthPage from './pages/AuthPage/AuthPage'
 
 function App() {
 
   return (
     <>
-      <h1>Welcome</h1>
-      <Button>Click me</Button>      
+      <Routes>
+        <Route path='/' element={<HomePage />}/>
+        <Route path='/auth' element={<AuthPage />}/>
+      </Routes>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import './index.css'
 import { ChakraProvider } from '@chakra-ui/react'
 import { extendTheme } from '@chakra-ui/react'
 import {mode} from '@chakra-ui/theme-tools'
+import { BrowserRouter } from 'react-router-dom'
 
 const styles = {
   global:(props) => ({
@@ -26,8 +27,10 @@ export default theme
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ChakraProvider theme={theme}>
-      <App />
-    </ChakraProvider>
+    <BrowserRouter>
+      <ChakraProvider theme={theme}>
+        <App />
+      </ChakraProvider>
+    </BrowserRouter>
   </React.StrictMode>,
 )
